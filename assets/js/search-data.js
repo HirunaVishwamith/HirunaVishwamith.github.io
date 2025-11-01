9e5c1177b947a590d32f4f3315ebb15a7275e046
HirunaVishwamith <69134587+HirunaVishwamith@users.noreply.github.com>
2025-11-01 13:26:53 +0000

    Deploying to gh-pages from @ HirunaVishwamith/HirunaVishwamith.github.io@7b41cbf6533962d90b880f8467602db41a125e2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,9 +431,9 @@ ninja.data = [{
           description: "RISC-V multi-core emulator capable of booting both Linux and bare-metal programs",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
